47252ccfd5c7cf1b4f172ac48710576b7c593069
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2026-02-12 15:47:36 -0600

    Initial Spring Boot project setup

---
+++ src/main/java/com/talkto/TalktoApplication.java
@@ -0,0 +1,13 @@
+package com.talkto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalktoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalktoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/talkto/TalktoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talkto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalktoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
