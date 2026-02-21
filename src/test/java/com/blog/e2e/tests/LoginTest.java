86d15801fd021c7b57d38be4407c47fe1dea9345
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2026-02-21 13:29:38 -0700

    Fix E2E LoginTest: make test self-contained (register user before login)

--- src/test/java/com/blog/e2e/tests/LoginTest.java
+++ src/test/java/com/blog/e2e/tests/LoginTest.java
@@ -2,13 +2,17 @@ package com.blog.e2e.tests;
 
 import com.blog.e2e.core.BaseTest;
 import com.blog.e2e.keywords.AuthKeywords;
+import com.blog.e2e.keywords.RegisterKeywords;
 import org.junit.jupiter.api.Test;
 
 public class LoginTest extends BaseTest {
 
     @Test
     void shouldLoginSuccessfully() {
+        RegisterKeywords register = new RegisterKeywords(driver, baseUrl);
         AuthKeywords auth = new AuthKeywords(driver, baseUrl);
-        auth.loginAs("register@gmail.com", "register12345");
+
+        String email = register.registerNewUser("Login User", "password123");
+        auth.loginAs(email, "password123");
     }
 }
